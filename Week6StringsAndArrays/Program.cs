4b770819e89ced310334d9e0c8bb72813416d760
Anneli Raamik. <anneli.raamik@tktk.ee>
2024-12-10 21:37:40 +0200

    add Week6StringsAndArrays code

---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,31 @@
+﻿string hello = "hello, world! Here I am? ";
+
+int stringLength = hello.Length;
+
+//string trimmedString = hello.Trim();
+
+hello = hello.Trim();
+
+char firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');
+
+
+firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+Console.WriteLine(hello);
+
+//int wordCounter = 1;    
+
+//for(int i = 0; i < hello.Length; i++)
+{
+    //Console.WriteLine($"{i} symbol in string: {hello[i]}");
+    //if (hello[i] =='')
+    //{  wordCounter++; }
+    //if (Char .IsDigit(hello[i]))
+    //{ wordCounter++; }
+}
+
+//Console.WriteLine($"Word count in {hello}: {wordCounter} words.");
+
